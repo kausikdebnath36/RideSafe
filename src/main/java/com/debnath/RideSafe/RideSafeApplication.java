beb05ab1f9fae0f76f67b4a585068c7d08e89bdb
kausik debnath <kausikdebna1920@gmail.com>
2024-10-01 09:37:13 +0530

    First Commit

---
+++ src/main/java/com/debnath/RideSafe/RideSafeApplication.java
@@ -0,0 +1,13 @@
+package com.debnath.RideSafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideSafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideSafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/debnath/RideSafe/RideSafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debnath.RideSafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideSafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
